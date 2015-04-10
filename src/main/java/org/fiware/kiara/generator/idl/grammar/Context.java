f8bca07379ffcecb4e93a3282402be8b38b2fc59
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2015-04-10 13:25:35 +0200

    Fixed comment.

--- src/main/java/org/fiware/kiara/generator/idl/grammar/Context.java
+++ src/main/java/org/fiware/kiara/generator/idl/grammar/Context.java
@@ -160,7 +160,7 @@ public class Context extends com.eprosima.idl.context.Context
     //! Cache the first exception.
     private com.eprosima.idl.parser.tree.Exception m_firstexception = null;
 
-    // TODO Lleva la cuenta de generaci�n de nuevos nombres.
+    // TODO Lleva la cuenta de generaci?n de nuevos nombres.
     private int m_randomGenName = 0;
     private Stack<String> m_randomGenNames = null;
     // TODO Lleva la cuenta del nombre de variables para bucles anidados.
